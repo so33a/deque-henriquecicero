52707aafa7e3333d006c0f75a4e34f6b555c81ab
Henrique Cícero <acicero@alunos.utfpr.edu.br>
2016-09-08 22:40:59 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -14,8 +14,10 @@ typedef struct {
 
 link novoNo(int item, link next);
 FILA novaFila();
-void inserir(FILA f, int e);
-int remover(FILA f);
+void inserirFinal(FILA f, int e);
+int removerInicio(FILA f);
+void inserirInicio(FILA f, int e);
+int removerFinal(FILA f);
 void imprimirFila(FILA f);
 void destroiFila(FILA f);
 int filaVazia(FILA f); 

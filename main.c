f7cc6274d81d83af84887a0b318c62732be23ea4
Henrique Cícero <acicero@alunos.utfpr.edu.br>
2016-09-14 00:22:55 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "fila.h"
 
-int main () {
+int main (void) {
   FILA alunos = novaFila();
+
   inserirFinal(alunos, 9);
   inserirFinal(alunos, 8);
   inserirFinal(alunos, 3);
   inserirFinal(alunos, 10);
+  inserirInicio(alunos, 1);
+  inserirFinal(alunos, 7);
+  inserirInicio(alunos, -1);
 
   imprimirFila(alunos);
 
+  //while(!filaVazia(alunos))
+    printf ("removido final: %d \n", removerFinal(alunos));
+  imprimirFila(alunos);
+
+  /*
   while(!filaVazia(alunos))
-    printf ("removido: %d \n", removerInicio(alunos));
+    printf ("removido inicio: %d \n", removerInicio(alunos));
+  imprimirFila(alunos);
+  */
 
-
-  inserirFinal(alunos, 10);
+  inserirFinal(alunos, 15);
   inserirFinal(alunos, 32);
-  
+
   imprimirFila(alunos);
 
   destroiFila(alunos);
-  return 0;
+
+  return EXIT_SUCCESS;
 }
